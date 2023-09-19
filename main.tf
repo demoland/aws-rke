6e9a65de243b47e73f7edf409ca53451e8198adc
Daniel Fedick <daniel.fedick@hashicorp.com>
2023-09-19 05:53:04 -0400

    Move to default vpc

--- main.tf
+++ main.tf
@@ -1,20 +1,43 @@
 locals {
-  cluster_name = "hashi-rke2"
-  aws_region   = "us-east-2"
+  cluster_name = "quickstart"
+  aws_region   = "us-gov-east-1"
 
   tags = {
     "terraform" = "true",
-    "env"       = "cloud-enabled",
+    "env"       = "quickstart",
   }
+  server_iam_role = "K8sUnrestrictedCloudProviderRole"
 }
 
-data "aws_ami" "centos8" {
+# Query for defaults
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet" "default" {
+  availability_zone = "${local.aws_region}a"
+  default_for_az    = true
+}
+
+# Private Key
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "pem" {
+  filename        = "${local.cluster_name}.pem"
+  content         = tls_private_key.ssh.private_key_pem
+  file_permission = "0600"
+}
+
+data "aws_ami" "rhel8" {
   most_recent = true
-  owners      = ["679593333241"]
+  owners      = ["219670896067"] # owner is specific to aws gov cloud
 
   filter {
     name   = "name"
-    values = ["CentOS*8*x86_64*"]
+    values = ["RHEL-8*"]
   }
 
   filter {
@@ -23,110 +46,35 @@ data "aws_ami" "centos8" {
   }
 }
 
-# Key Pair
-resource "tls_private_key" "ssh" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "ssh_pem" {
-  filename        = "${local.cluster_name}.pem"
-  content         = tls_private_key.ssh.private_key_pem
-  file_permission = "0600"
-}
-
-#
-# Network
-#
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "rke2-${local.cluster_name}"
-  cidr = "10.88.0.0/16"
-
-  azs             = ["${local.aws_region}a", "${local.aws_region}b", "${local.aws_region}c"]
-  public_subnets  = ["10.88.1.0/24", "10.88.2.0/24", "10.88.3.0/24"]
-  private_subnets = ["10.88.101.0/24", "10.88.102.0/24", "10.88.103.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_vpn_gateway   = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  # Add in required tags for proper AWS CCM integration
-  public_subnet_tags = merge({
-    "kubernetes.io/cluster/${module.rke2.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                            = "1"
-  }, local.tags)
-
-  private_subnet_tags = merge({
-    "kubernetes.io/cluster/${module.rke2.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"                   = "1"
-  }, local.tags)
-
-  tags = merge({
-    "kubernetes.io/cluster/${module.rke2.cluster_name}" = "shared"
-  }, local.tags)
-}
-
 #
 # Server
 #
 module "rke2" {
-  source = "./aws-rke2"
-
-  cluster_name = local.cluster_name
-  vpc_id       = module.vpc.vpc_id
-  subnets      = module.vpc.public_subnets # Note: Public subnets used for demo purposes, this is not recommended in production
-
-  ami                   = data.aws_ami.centos8.image_id # Note: Multi OS is primarily for example purposes
+  source                = "./aws-rke2"
+  cluster_name          = local.cluster_name
+  vpc_id                = data.aws_vpc.default.id
+  subnets               = [data.aws_subnet.default.id]
+  ami                   = data.aws_ami.rhel8.image_id
   ssh_authorized_keys   = [tls_private_key.ssh.public_key_openssh]
-  instance_type         = "t3a.medium"
+  iam_instance_profile  = local.server_iam_role
   controlplane_internal = false # Note this defaults to best practice of true, but is explicitly set to public for demo purposes
-  servers               = 1
-
-  # Enable AWS Cloud Controller Manager
-  enable_ccm = true
-
-  rke2_config = <<-EOT
-node-label:
-  - "name=server"
-  - "os=centos8"
-EOT
-
-  tags = local.tags
+  tags                  = local.tags
 
 }
 
 #
-# Generic agent pool
+# Generic Agent Pool
 #
 module "agents" {
-  source = "./aws-rke2/modules/agent-nodepool"
-
-  name    = "generic"
-  vpc_id  = module.vpc.vpc_id
-  subnets = module.vpc.public_subnets # Note: Public subnets used for demo purposes, this is not recommended in production
-
-  ami                 = data.aws_ami.centos8.image_id # Note: Multi OS is primarily for example purposes
+  source              = "./aws-rke2/modules/agent-nodepool"
+  name                = "generic"
+  vpc_id              = data.aws_vpc.default.id
+  subnets             = [data.aws_subnet.default.id]
+  ami                 = data.aws_ami.rhel8.image_id
   ssh_authorized_keys = [tls_private_key.ssh.public_key_openssh]
-  spot                = true
-  asg                 = { min : 1, max : 10, desired : 2 }
-  instance_type       = "t3a.large"
+  tags                = local.tags
+  cluster_data        = module.rke2.cluster_data
 
-  # Enable AWS Cloud Controller Manager and Cluster Autoscaler
-  enable_ccm        = true
-  enable_autoscaler = true
-
-  rke2_config = <<-EOT
-node-label:
-  - "name=generic"
-  - "os=centos8"
-EOT
-
-  cluster_data = module.rke2.cluster_data
-  tags         = local.tags
 }
 
 # For demonstration only, lock down ssh access in production
